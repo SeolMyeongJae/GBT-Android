69fa0590765ae8f1126c16e82b6cdc50cc9e138e
MinKi1506 <lunaris4537@gmail.com>
2022-03-26 15:35:08 +0900

    second commit

---
+++ GBT_4/app/src/main/java/com/example/gbt_4/InputInfo.java
@@ -0,0 +1,29 @@
+package com.example.gbt_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InputInfo extends AppCompatActivity {
+
+    Button btn_submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_info);
+
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainPage.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
