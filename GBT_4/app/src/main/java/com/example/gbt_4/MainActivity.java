72c03e7753b89042341cf65b8cff0b18604e53ef
MinKi1506 <lunaris4537@gmail.com>
2022-03-26 14:30:13 +0900

    first commit

---
+++ GBT_4/app/src/main/java/com/example/gbt_4/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.gbt_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LogIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
