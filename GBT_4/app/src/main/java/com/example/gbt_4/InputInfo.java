7fd93df279814aa37e274b25598405fe95801107
MinKi1506 <lunaris4537@gmail.com>
2022-03-29 16:32:49 +0900

    Third commit

--- GBT_4/app/src/main/java/com/example/gbt_4/InputInfo.java
+++ GBT_4/app/src/main/java/com/example/gbt_4/InputInfo.java
@@ -2,19 +2,50 @@ package com.example.gbt_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class InputInfo extends AppCompatActivity {
 
-    Button btn_submit;
+    Button btn_submit, btn_select_year;
+    TextView tv_birthYear;
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener(){
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+        Log.d("YearMonthPickerTest", "year = " + year + ", month = " + monthOfYear + ", day = " + dayOfMonth);
+            String yearStr = Integer.toString(year);
+            tv_birthYear.setText(yearStr);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_info);
+
+        tv_birthYear = (TextView) findViewById(R.id.tv_birthYear);
+
+        btn_select_year = findViewById(R.id.btn_select_year);
+        btn_select_year.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                SelectYear selectYear = new SelectYear();
+                selectYear.setListener(d);
+                selectYear.show(getSupportFragmentManager(),"출생년도 선택");
+//                tv_birthYear.setText(SelectYear.year);
+//                String year = Integer.toString(selectYear.birthYear);
+//                tv_birthYear.setText(year);
+
+            }
+        });
 
         btn_submit = (Button) findViewById(R.id.btn_submit);
         btn_submit.setOnClickListener(new View.OnClickListener() {
