b44700947514ff230d6d7a4576e3e5151a604d58
MinKi1506 <lunaris4537@gmail.com>
2022-04-18 19:31:05 +0900

    화면구현

--- GBT_4/app/src/main/java/com/example/gbt_4/adapter/InviteAdapter.kt
+++ GBT_4/app/src/main/java/com/example/gbt_4/adapter/InviteAdapter.kt
@@ -8,13 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.gbt_4.InviteNotice
 import com.example.gbt_4.R
 import com.example.gbt_4.databinding.ActivityNoticeBinding
-import com.example.gbt_4.databinding.InviteBinding
+import com.example.gbt_4.databinding.ItemInviteBinding
 
 class InviteAdapter : RecyclerView.Adapter<InviteAdapter.ViewHolder>() {
     var invites = ArrayList<InviteNotice>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = InviteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemInviteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
@@ -40,7 +40,7 @@ class InviteAdapter : RecyclerView.Adapter<InviteAdapter.ViewHolder>() {
 
     override fun getItemCount() = invites.size
 
-    inner class ViewHolder(val binding: InviteBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemInviteBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun setItem(invite: InviteNotice) {
             binding.tvCaller.text = invite.caller
